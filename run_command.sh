493e0c70b9650d3798ffbd0ed14d822a05c32be8
Yufeng Xu <yx3038@nyu.edu>
2025-07-26 02:36:27 +0000

    update run_command

--- run_command.sh
+++ run_command.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-cd /home/zephyr/gcs-bucket/pruning
-git clone -b minitron-tpu git@github.com:jiachenzhu/fms.git fms-grad-accum
+# cd /home/zephyr/gcs-bucket/pruning
+# git clone -b minitron-tpu git@github.com:jiachenzhu/fms.git fms-grad-accum
+
 bash ~/gcs-bucket/pruning/fms-grad-accum/scripts/finetuning.sh
--- start.sh
+++ start.sh
@@ -10,25 +10,30 @@ source config.sh
 #     --preemptible; \
 # do : ; done
 
+# gcloud alpha compute tpus tpu-vm ssh $TPU_NAME \
+#   --zone=$ZONE \
+#   --ssh-key-file='~/.ssh/id_rsa' \
+#   --worker=all \
+#   --command "
+#   cd ~
+#   git clone -b main https://github.com/Zephyr271828/TPU-VM-setup.git
+#   cd TPU-VM-setup
+#   git pull origin main
+
+#   bash setup_conda.sh
+
+#   export bucket_name=$BUCKET_NAME
+#   export bucket_dir=$BUCKET_DIR
+#   bash setup_gcsfuse.sh
+
+#.  bash start.sh
+#   "
+
 gcloud alpha compute tpus tpu-vm ssh $TPU_NAME \
   --zone=$ZONE \
   --ssh-key-file='~/.ssh/id_rsa' \
   --worker=all \
   --command "
-  cd ~
-  git clone -b main https://github.com/Zephyr271828/TPU-VM-setup.git
-  cd TPU-VM-setup
-  git pull origin main
-
-  export bucket_name=$BUCKET_NAME
-  export bucket_dir=$BUCKET_DIR
-
-  bash setup_conda.sh
-  bash setup_gcsfuse.sh
+  cd ~/TPU-VM-setup
+  bash run_command.sh
   "
-
-# gcloud alpha compute tpus tpu-vm ssh $TPU_NAME \
-#   --zone=$ZONE \
-#   --ssh-key-file='~/.ssh/id_rsa' \
-#   --worker=all \
-#   --command "bash run_command.sh"
