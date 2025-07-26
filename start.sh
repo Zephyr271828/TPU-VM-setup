36fcae36fe022fb5c9d493f1d2ba02e4eda872a9
Yufeng Xu <yx3038@nyu.edu>
2025-07-26 02:36:51 +0000

    update run_command

--- start.sh
+++ start.sh
@@ -10,30 +10,22 @@ source config.sh
 #     --preemptible; \
 # do : ; done
 
-# gcloud alpha compute tpus tpu-vm ssh $TPU_NAME \
-#   --zone=$ZONE \
-#   --ssh-key-file='~/.ssh/id_rsa' \
-#   --worker=all \
-#   --command "
-#   cd ~
-#   git clone -b main https://github.com/Zephyr271828/TPU-VM-setup.git
-#   cd TPU-VM-setup
-#   git pull origin main
-
-#   bash setup_conda.sh
-
-#   export bucket_name=$BUCKET_NAME
-#   export bucket_dir=$BUCKET_DIR
-#   bash setup_gcsfuse.sh
-
-#.  bash start.sh
-#   "
-
 gcloud alpha compute tpus tpu-vm ssh $TPU_NAME \
   --zone=$ZONE \
   --ssh-key-file='~/.ssh/id_rsa' \
   --worker=all \
   --command "
-  cd ~/TPU-VM-setup
-  bash run_command.sh
+  cd ~
+  git clone -b main https://github.com/Zephyr271828/TPU-VM-setup.git
+  cd TPU-VM-setup
+  git pull origin main
+
+  # bash setup_conda.sh
+
+  # export bucket_name=$BUCKET_NAME
+  # export bucket_dir=$BUCKET_DIR
+  # bash setup_gcsfuse.sh
+
+  bash start.sh
   "
+
